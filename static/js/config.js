97edfc40c067ba026523e5402e3d2270cec95449
gelopez21 <gelopez1984@gmail.com>
2020-12-07 04:49:35 -0600

    adding config

---
+++ static/js/config.js
@@ -0,0 +1,5 @@
+// API key
+const API_KEY = "pk.eyJ1IjoiZXJlc2RpdmluYSIsImEiOiJja2hwa2w4OWMwMHVsMnJvOGE3ZDIxdHlwIn0.PfbFm_UP5Rw2C66p0x9Eng";
+
+// Endpoint for M2.5+ earthquakes within the month
+var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_month.geojson"
